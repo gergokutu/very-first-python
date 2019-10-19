b6efcde26251205f27ecd69753c6cc00c1e641c9
gergokutu <gergo.kutu@gmail.com>
2019-10-19 21:34:51 +0200

    Same with Python and vanillaJS

---
+++ sameWithJS.js
@@ -0,0 +1,3 @@
+for (let i = 0; i < 5; i++) {
+  console.log("Let's start it! :) - written with vanillaJS")
+}
---
+++ start.py
@@ -0,0 +1,2 @@
+for i in range(5):
+  print("Let's start it! :) - written in Python3.8")
