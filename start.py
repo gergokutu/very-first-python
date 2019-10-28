6e813106ab3f858208d05d99a42d44747e39e69b
gergokutu <gergo.kutu@gmail.com>
2019-10-28 17:59:40 +0100

    My first variables

--- start.py
+++ start.py
@@ -12,11 +12,13 @@ print("  / |")
 print(" /  |")
 print("/___|")
 
-# variables » only: variable_name = "value"
+# variables > only: variable_name = "value"
 character_name = "Pocok"
 character_age = "35"
 
 print("There once was a man named " + character_name + ", ")
 print("he was " + character_age + " years old.")
+
+character_name = "Pista"
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age + ".")
