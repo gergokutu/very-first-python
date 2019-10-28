0283460b5db8532a31ed28d2a7d6f81d59549306
gergokutu <gergo.kutu@gmail.com>
2019-10-28 17:57:33 +0100

    My first variables

--- start.py
+++ start.py
@@ -5,3 +5,18 @@ for i in range(5):
 # for (let i = 0; i < 5; i++) {
 #   console.log("Let's start it! :) - written with vanillaJS")
 # }
+
+# draw triangle
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+# variables » only: variable_name = "value"
+character_name = "Pocok"
+character_age = "35"
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old.")
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
