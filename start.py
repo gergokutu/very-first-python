d9dd598fc368731e8662a94c2359db0533d2e61f
gergokutu <gergo.kutu@gmail.com>
2019-10-29 15:06:39 +0100

    Strings

--- start.py
+++ start.py
@@ -6,14 +6,13 @@ for i in range(5):
 #   console.log("Let's start it! :) - written with vanillaJS")
 # }
 
-# d
-# Draw triangle
+## Draw triangle
 print("   /|")
 print("  / |")
 print(" /  |")
 print("/___|")
 
-# Variables >> only: variable_name = "value"
+## Variables >> only: variable_name = "value"
 character_name = "Pocok"
 # Not character_age = 35.5 >> cannot concatenate 'str' and 'int' objects...
 character_age = "35.5"
@@ -27,4 +26,38 @@ character_name = "Pista"
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age + ".")
 
-# Working with strings
+## Working with strings
+# Escape character >> \ << "\n"...
+print("Pocok\nAcademy")
+print("Pocok\"s Academy")
+
+phrase = "Let's do it!"
+phrase_two = "TWO"
+# Concatenation >> +
+print(phrase + " Now!!!")
+# lower(), islower()...
+print(phrase + " Now!!!".lower())
+print(phrase.lower() + " Now!!!")
+print(phrase.lower() + " NOOW!!!".lower())
+
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.islower())
+print(phrase_two.isupper())
+
+print(phrase_two.lower().islower())
+
+# len >> lenght
+print(len(phrase))
+
+# Grab the first character
+print(phrase[0])
+
+# Grab the index
+print(phrase.index("L"))
+print(phrase.index("Let"))
+
+# Replace do >> concatenate
+print(phrase.replace("do", "concatenate"))
+
+
