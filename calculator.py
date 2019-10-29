4e5229c4a5dee46816df66bf4c3ecfd76e98fde9
gergokutu <gergo.kutu@gmail.com>
2019-10-29 15:57:39 +0100

    Calculator adding up decimal decimal user inputs

---
+++ calculator.py
@@ -0,0 +1,10 @@
+## Building a basic calculator
+# By default the input is string
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+# string to int / float
+# result = int(num1) + int(num2)
+# To be able to input decimals
+result = float(num1) + float(num2)
+
+print(result)
