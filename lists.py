cb9376f47f92eab48aced8b93e60920b0c070053
gergokutu <gergo.kutu@gmail.com>
2019-10-29 16:24:43 +0100

    Grab values from the array

---
+++ lists.py
@@ -0,0 +1,16 @@
+## Lists (arrays)
+# create it
+mixed_list = ["Riki", "Tiki", "Viki", 3, False]
+print(mixed_list)
+
+friends = ["Riki", "Tiki", "Viki", "Dago", "Kvak"]
+
+# Grab values from the list
+print(friends)
+print(friends[0])
+print(friends[-1])
+print(friends[-2])
+print(friends[1:])
+print(friends[1:4])
+
+# Modify
