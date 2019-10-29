40a67f7b1897c48bfd19ef6ea2a714f738892dda
gergokutu <gergo.kutu@gmail.com>
2019-10-29 16:13:17 +0100

    Print out user input in sentences

--- game1.py
+++ game1.py
@@ -1,4 +1,9 @@
 ## Mad libs game
-print("Roses are red")
-print("Violets are blue")
-print("I love you")
+# Random words pops up in the sentences
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
