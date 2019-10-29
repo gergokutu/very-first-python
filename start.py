37e3ce90a014042ce8c02cf3082041fb7c07d0c5
gergokutu <gergo.kutu@gmail.com>
2019-10-29 14:42:06 +0100

    String, number, boolean

--- start.py
+++ start.py
@@ -1,23 +1,24 @@
 for i in range(5):
   print("Hello Mogyi! :) - written in Python3.8")
 
-# same with Javascript
+# Same with Javascript
 # for (let i = 0; i < 5; i++) {
 #   console.log("Let's start it! :) - written with vanillaJS")
 # }
 
-# draw triangle
+# d
+# Draw triangle
 print("   /|")
 print("  / |")
 print(" /  |")
 print("/___|")
 
-# variables >> only: variable_name = "value"
+# Variables >> only: variable_name = "value"
 character_name = "Pocok"
-# not character_age = 35.5 >> cannot concatenate 'str' and 'int' objects...
+# Not character_age = 35.5 >> cannot concatenate 'str' and 'int' objects...
 character_age = "35.5"
 # True needs to be capitol
-# not isMale but is_male
+# Not isMale but is_male
 is_male = True 
 print("There once was a man named " + character_name + ", ")
 print("he was " + character_age + " years old.")
@@ -25,3 +26,5 @@ print("he was " + character_age + " years old.")
 character_name = "Pista"
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age + ".")
+
+# Working with strings
