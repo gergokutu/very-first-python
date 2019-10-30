c5f422a3d708a143a92665eaa407cdad100253cd
gergokutu <gergo.kutu@gmail.com>
2019-10-30 16:50:42 +0100

    Parameters in functions

--- functions.py
+++ functions.py
@@ -10,3 +10,14 @@ print("- before -")
 # call it
 say_hi() 
 print("- after -")
+
+# parameters >> e.g. name, age
+def say_hello(name, age):
+  print("What's up " + name + "?\nYou are under " + str(age) + "? Huhh?\n\n")
+
+say_hello("Pista", 21)
+
+name = "Lulu"
+age = 18
+say_hello(name, age)
+say_hello("Bözske", 45)
