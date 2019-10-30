9bb4a15c40d7cc8c3e148b2832ea86dcb9bee010
gergokutu <gergo.kutu@gmail.com>
2019-10-30 16:38:59 +0100

    Functions

---
+++ functions.py
@@ -0,0 +1,12 @@
+## Functions
+# collection of code
+# which performs a specific task
+
+def say_hi():
+  # next line has to be intended
+  print("Hello User")
+
+print("- before -")
+# call it
+say_hi() 
+print("- after -")
