912979d231d6039da7bc32a0eeb30a6391928664
gergokutu <gergo.kutu@gmail.com>
2019-10-30 16:24:44 +0100

    Tuples

---
+++ tuples.py
@@ -0,0 +1,17 @@
+## Tuples
+# similar to lists
+# but key differences
+# create tuples >> ()
+coordinates = (4, 5)
+print(coordinates[0])
+
+# tuples are immutable (cannot change...)
+# coordinates[1] = 10 # error
+# use for data that never gonna change!
+
+# list of tuples
+coordinates = [(4, 5), (6, 7), (8, 9)]
+print(coordinates[1])
+print(len(coordinates[1]))
+
+
