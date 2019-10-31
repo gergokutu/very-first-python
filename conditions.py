e9400077db60a271d4a7c8ab225dd1915899e2d1
gergokutu <gergo.kutu@gmail.com>
2019-10-31 13:41:36 +0100

    Comparing with if statement

--- conditions.py
+++ conditions.py
@@ -1,9 +1,9 @@
-## If statement
+## If statement to decide
 # basic
 is_male = False # change it to False and run
 is_tall = False # change it to False and run
 
-# : and indentation%, (), and/or...
+# : and indentation, not(), and/or...
 if is_male and is_tall:
   print("You are both male and tall")
 # if is_male or is_tall:
@@ -15,3 +15,16 @@ elif not(is_male) and is_tall:
   print("You are short and not male")
 else:
   print("You are either not male or not tall")
+
+## If statements to compare
+# Pass three numbers to the function
+# It gives back the maximum number
+def max_num(num1, num2, num3):
+  if num1 >= num2 and num1 >= num3:
+    return num1
+  elif num2 >= num1 and num2 >= num3:
+    return num2
+  else:
+    return num3
+
+print(max_num(1, 6, 2))
