27b9b3bc4100407856ede02f6300aa4c96e2c7cb
gergokutu <gergo.kutu@gmail.com>
2019-10-31 16:14:19 +0100

    Translator

---
+++ translator.py
@@ -0,0 +1,29 @@
+# Pocok language
+# vowels >> p
+# --------------
+# dog >> dpg
+# cat >> cpt
+
+def translate(phrase):
+  translation = ""
+  for letter in phrase:
+    # check if the letter is a vowel
+    if letter in "AEIOUaeiou":
+      translation = translation + "p"
+    else:
+      translation = translation + letter
+  return translation
+
+print(translate(input("Enter a phrase for translate func: ")))
+
+def translate2(phrase):
+  translation = []
+  for letter in phrase:
+    # check if the letter is a vowel
+    if letter.lower() in "aeiou":
+      translation.append("p")
+    else:
+      translation.append(letter)
+  return translation
+
+print(translate2(input("Enter a phrase for translate2 func: ")))
