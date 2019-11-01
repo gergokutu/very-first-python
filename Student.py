54f4530b5f33476ffba88e8ee97771de79f191a4
gergokutu <gergo.kutu@gmail.com>
2019-11-01 12:55:19 +0100

    Student class

---
+++ Student.py
@@ -0,0 +1,9 @@
+class Student:
+    # define the Student data type
+    def __init__(self, name, major, points, is_on_probation):
+        # assign some values
+        self.name = name
+        # the major of the object we passed in >> major
+        self.major = major
+        self.points = points
+        self.is_on_probation = is_on_probation
---
+++ class_objects.py
@@ -0,0 +1,15 @@
+## Classes & Objects
+# to create your own data types >> Student.py
+# from file import class
+from Student import Student
+
+# create an actual student >> object
+# name, major...
+student1 = Student("Pista", "Business", 3.1, False)
+student2 = Student("Joli", "Art", 3.5, True)
+
+print(student1.name)
+print(student2.is_on_probation)
+
+# you can do objects and classes with anything...
+# you can model real world objects
