e69972e40d3bfe09ac23b624f7e4cdae27d9a27e
gergokutu <gergo.kutu@gmail.com>
2019-12-12 20:36:03 +0100

    Modify student2 in class_objects.py

--- class_objects.py
+++ class_objects.py
@@ -7,10 +7,10 @@ from Student2 import Student2
 # create an actual student >> object
 # name, major...
 student1 = Student("Pista", "Business", 3.1, False)
-student2 = Student2("Joli", "Art", 3.5)
+student2 = Student("Joli", "Art", 3.5, True)
 
 print(student1.name)
-print(student2.on_honor_roll())
+print(student2.is_on_probation)
 
 # you can do objects and classes with anything...
 # you can model real world objects
