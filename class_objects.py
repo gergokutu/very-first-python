87df9a00ff41d3a773daf141f507115e1864eba9
gergokutu <gergo.kutu@gmail.com>
2019-12-12 18:11:45 +0100

    Student2 class used in class_objects.py

--- class_objects.py
+++ class_objects.py
@@ -2,14 +2,15 @@
 # to create your own data types >> Student.py
 # from file import class
 from Student import Student
+from Student2 import Student2
 
 # create an actual student >> object
 # name, major...
 student1 = Student("Pista", "Business", 3.1, False)
-student2 = Student("Joli", "Art", 3.5, True)
+student2 = Student2("Joli", "Art", 3.5)
 
 print(student1.name)
-print(student2.is_on_probation)
+print(student2.on_honor_roll)
 
 # you can do objects and classes with anything...
 # you can model real world objects
